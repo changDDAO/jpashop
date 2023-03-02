dfddd08086589fa71a02d218379a0e774b41b11a
Changhogod <younch8342@naver.com>
2023-03-02 21:37:06 +0900

    database init & create tables

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop;
 
 
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.MemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
